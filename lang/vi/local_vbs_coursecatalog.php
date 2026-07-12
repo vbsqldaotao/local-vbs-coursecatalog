7480495156d619de714c32105b3cefdfd73ff077
Kiểm thử viên <thanghps@gmail.com>
2026-07-12 13:20:50 +0000

    feat(VBS-357): add Vietnamese language pack for local_vbs_coursecatalog
    Co-authored-by: multica-agent <github@multica.ai>

---
+++ lang/vi/local_vbs_coursecatalog.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Vietnamese language strings for local_vbs_coursecatalog.
+ *
+ * @package     local_vbs_coursecatalog
+ * @copyright   2026 VBS Đào tạo
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname']  = 'Danh mục khóa học VBS';
+$string['nocourses']   = 'Không có khóa học nào.';
+$string['startdate']   = 'Ngày bắt đầu';
+$string['enddate']     = 'Ngày kết thúc';
+$string['viewcourse']  = 'Xem khóa học';
+$string['privacy:metadata'] = 'Plugin Danh mục khóa học VBS không lưu trữ bất kỳ dữ liệu cá nhân nào.';
